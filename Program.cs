3de0c634d2cde7264daf02980add395d89fc6cb5
aftabpatel10 <aftabpatel3107@gmail.com>
2022-11-22 09:20:13 +0530

    Checking Fibonacci Series Preogram

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Logical Programs");
+            Console.WriteLine("Fibonacci Program");
+            Console.WriteLine("Enter the number of elements:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int n1 = 0, n2 = 1, n3;
+            Console.Write(n1 + " " + n2 + " ");
+            for (int i = 2; i < n; i++)
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
