1466ddfe944a22cddc5f41ba5bee2672af4f8432
aftabpatel10 <aftabpatel3107@gmail.com>
2022-11-22 10:06:28 +0530

    Checking a Program For Monthly Payment

--- Program.cs
+++ Program.cs
@@ -11,17 +11,22 @@ namespace LogicalPrograme
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("SquareRoot Program");
-            Console.WriteLine("Enter the number to find squareroot:");
-            int num = Convert.ToInt32(Console.ReadLine());
-            float squareRoot = num / 2;
-            float temp = 0;
-            while (squareRoot != temp)
-            {
-                temp = squareRoot;
-                squareRoot = (num / temp + temp) / 2;
-            }
-            Console.WriteLine($"Square Root of {num} is :" + squareRoot);
+            Console.WriteLine("Monthly Payment  Program");
+            Console.WriteLine("Please enter the loan amount you want to have");
+            double P = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter the loan time in years:");
+            double Y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Rate of Interest will be :");
+            double R = Convert.ToDouble(Console.ReadLine());
+            //formulation steps
+            double n = 12 * Y;
+            double r = R / (12 * 100);
+            double upsideCalculation = P * r;
+            double downsideAddition = 1 + r;
+            double downsideRaisedToCalculation = Math.Pow(downsideAddition, -n);
+            double downsideCalculation = 1 - downsideRaisedToCalculation;
+            double monthlyPayment = upsideCalculation / downsideCalculation;
+            Console.WriteLine($"Monthly Payment will be : {monthlyPayment}");
             Console.ReadLine();
         }
     }
